04fb24a36c9d641ca2b6fa0c828dd82cbda77226
BooleanCat <thgodkin@gmail.com>
2017-07-27 22:03:29 +0100

    some lint fixes

--- test/test_quicktable.py
+++ test/test_quicktable.py
@@ -5,10 +5,12 @@ import quicktable
 def test_empty_table_has_len_0():
     assert len(quicktable.Table()) == 0
 
+
 def test_append_increments_length():
     table = quicktable.Table()
     table.append()
     assert len(table) == 1
+
 
 def test_append_increments_length_twice():
     table = quicktable.Table()
@@ -16,11 +18,13 @@ def test_append_increments_length_twice():
     table.append()
     assert len(table) == 2
 
+
 def test_pop_decrements_length():
     table = quicktable.Table()
     table.append()
     table.pop()
     assert len(table) == 0
+
 
 def test_pop_decrements_length_twice():
     table = quicktable.Table()
@@ -29,6 +33,7 @@ def test_pop_decrements_length_twice():
     table.pop()
     table.pop()
     assert len(table) == 0
+
 
 def test_pop_empty_table_raises_error():
     table = quicktable.Table()
