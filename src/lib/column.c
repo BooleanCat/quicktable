c546856508759f22921c4ea042ac80b2f03a7525
BooleanCat <thgodkin@gmail.com>
2017-08-05 19:09:47 +0100

    refactor column init

--- src/lib/column.c
+++ src/lib/column.c
@@ -1,37 +1,58 @@
 #include "column.h"
 
-bool qtab_Column_init(qtab_Column *column, PyObject *descriptor) {
-  PyObject *name;
-  PyObject *type;
+static bool qtab_Column_init_name(qtab_Column *column, PyObject *name) {
   char *name_s;
-  char *type_s;
-
-  name = PySequence_ITEM(descriptor, 0);
-  if (name == NULL)
-    return false;
 
   name_s = PyUnicode_AsUTF8AndSize(name, NULL);
   if (name == NULL)
     return false;
 
   column->name = strdup(name_s);
-  Py_DECREF(name);
+  if (column->name == NULL) {
+    PyErr_SetString(PyExc_MemoryError, "failed to initialise column");
+    return false;
+  }
 
-  if (column->name == NULL)
+  return true;
+}
+
+static bool qtab_Column_init_type(qtab_Column *column, PyObject *type) {
+  char *type_s;
+
+  type_s = PyUnicode_AsUTF8AndSize(type, NULL);
+  if (type == NULL)
+    return false;
+
+  column->type = strdup(type_s);
+  if (column->type == NULL) {
+    PyErr_SetString(PyExc_MemoryError, "failed to initialise column");
+    return false;
+  }
+
+  return true;
+}
+
+bool qtab_Column_init(qtab_Column *column, PyObject *descriptor) {
+  PyObject *name;
+  PyObject *type;
+  bool success;
+
+  name = PySequence_ITEM(descriptor, 0);
+  if (name == NULL)
+    return false;
+
+  success = qtab_Column_init_name(column, name);
+  Py_DECREF(name);
+  if (success == false)
     return false;
 
   type = PySequence_ITEM(descriptor, 1);
   if (type == NULL)
     return false;
 
-  type_s = PyUnicode_AsUTF8AndSize(type, NULL);
-  if (type_s == NULL)
-    return false;
-
-  column->type = strdup(type_s);
+  success = qtab_Column_init_type(column, type);
   Py_DECREF(type);
-
-  if (column->type == NULL)
+  if (success == false)
     return false;
 
   return true;
