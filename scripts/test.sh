7a37cd50f57b0f91e778b49ab0c185c8abc962eb
BooleanCat <thgodkin@gmail.com>
2017-08-05 13:51:22 +0100

    compile with python flags

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+export PY_CFLAGS="$( python-config --cflags )"
+export PY_LDFLAGS="$( python-config --ldflags )"
+
 pushd test/c >/dev/null
-  rm -f qtab_*
+  rm -fr qtab_*
   make qtab_test_column
   ./qtab_test_column
 popd >/dev/null
--- test/c/test_column.c
+++ test/c/test_column.c
@@ -2,12 +2,22 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include <Python.h>
 
 static void null_test_success(void **state) {
   (void) state;
 }
 
 int main(void) {
+  Py_Initialize();
+  PyGILState_STATE gstate;
+  gstate = PyGILState_Ensure();
+
+  PyObject *list = PyList_New(0);
+  Py_DECREF(list);
+
+  PyGILState_Release(gstate);
+
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(null_test_success),
   };
