77b703769425e073edba205c4a9869f061576611
BooleanCat <thgodkin@gmail.com>
2017-09-03 14:38:51 +0100

    dry out tests a bit

--- test/py/test_quicktable.py
+++ test/py/test_quicktable.py
@@ -11,33 +11,29 @@ def test_empty_table_has_len_0(empty_table):
     assert len(empty_table) == 0
 
 
-def test_append_increments_length():
-    table = quicktable.Table([])
-    table.append()
-    assert len(table) == 1
+def test_append_increments_length(empty_table):
+    empty_table.append()
+    assert len(empty_table) == 1
 
 
-def test_append_increments_length_twice():
-    table = quicktable.Table([])
-    table.append()
-    table.append()
-    assert len(table) == 2
+def test_append_increments_length_twice(empty_table):
+    empty_table.append()
+    empty_table.append()
+    assert len(empty_table) == 2
 
 
-def test_pop_decrements_length():
-    table = quicktable.Table([])
-    table.append()
-    table.pop()
-    assert len(table) == 0
+def test_pop_decrements_length(empty_table):
+    empty_table.append()
+    empty_table.pop()
+    assert len(empty_table) == 0
 
 
-def test_pop_decrements_length_twice():
-    table = quicktable.Table([])
-    table.append()
-    table.append()
-    table.pop()
-    table.pop()
-    assert len(table) == 0
+def test_pop_decrements_length_twice(empty_table):
+    empty_table.append()
+    empty_table.append()
+    empty_table.pop()
+    empty_table.pop()
+    assert len(empty_table) == 0
 
 
 def test_pop_empty_table_raises_exception(empty_table):
